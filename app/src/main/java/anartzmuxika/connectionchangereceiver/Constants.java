57ae45b354c26d5d18d718164df818c1bb06e14d
Anartz Mugika <mugan86@gmail.com>
2016-06-22 08:31:48 +0200

    Add constants to add values about connection status

---
+++ app/src/main/java/anartzmuxika/connectionchangereceiver/Constants.java
@@ -0,0 +1,12 @@
+package anartzmuxika.connectionchangereceiver;
+
+/**
+ * Created by anartzmugika on 22/6/16.
+ */
+
+public class Constants {
+
+    public static final String CONNECT_TO_WIFI = "WIFI";
+    public static final String CONNECT_TO_MOBILE = "MOBILE";
+    public static final String NOT_CONNECT = "NOT_CONNECT";
+}
