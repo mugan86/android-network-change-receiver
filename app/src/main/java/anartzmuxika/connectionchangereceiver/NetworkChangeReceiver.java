bc504a3c2a04234ab90203153493523f06c0919a
Anartz Muxika Ledo <mugan86@gmail.com>
2016-11-02 10:46:26 +0100

    Log text correct

--- app/src/main/java/anartzmuxika/connectionchangereceiver/NetworkChangeReceiver.java
+++ app/src/main/java/anartzmuxika/connectionchangereceiver/NetworkChangeReceiver.java
@@ -20,7 +20,7 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
         Log.e("Receiver ", "" + status);
 
         if (status.equals(Constants.NOT_CONNECT)) {
-            Log.e("Receiver ", "not connction");// your code when internet lost
+            Log.e("Receiver ", "not connection");// your code when internet lost
 
 
         } else {
