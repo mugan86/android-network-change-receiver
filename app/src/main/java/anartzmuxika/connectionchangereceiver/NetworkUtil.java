fb9e104b1f63d26cb51171e49e0b8b856278e6c4
Anartz Muxika Ledo <mugan86@gmail.com>
2016-11-02 10:46:54 +0100

    Check mobile connection is 3G, 4G or other type

--- app/src/main/java/anartzmuxika/connectionchangereceiver/NetworkUtil.java
+++ app/src/main/java/anartzmuxika/connectionchangereceiver/NetworkUtil.java
@@ -3,6 +3,7 @@ package anartzmuxika.connectionchangereceiver;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
 
 /**
  * Created by anartzmugika on 22/6/16.
@@ -42,10 +43,45 @@ public class NetworkUtil {
             status = Constants.CONNECT_TO_WIFI;
         } else if (conn == NetworkUtil.TYPE_MOBILE) {
             //status = "Mobile data enabled";
-            status = Constants.CONNECT_TO_MOBILE;
+            status = getNetworkClass(context);
         } else if (conn == NetworkUtil.TYPE_NOT_CONNECTED) {
             status = Constants.NOT_CONNECT;
         }
         return status;
+    }
+
+    public static String getNetworkClass(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if(info==null || !info.isConnected())
+            return "-"; //not connected
+        if(info.getType() == ConnectivityManager.TYPE_WIFI)
+            return "WIFI";
+        if(info.getType() == ConnectivityManager.TYPE_MOBILE){
+            int networkType = info.getSubtype();
+            switch (networkType) {
+                /*case TelephonyManager.NETWORK_TYPE_GPRS:
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                case TelephonyManager.NETWORK_TYPE_IDEN: //api<8 : replace by 11
+                    return "2G";
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                case TelephonyManager.NETWORK_TYPE_EVDO_B: //api<9 : replace by 14
+                case TelephonyManager.NETWORK_TYPE_EHRPD:  //api<11 : replace by 12*/
+                case TelephonyManager.NETWORK_TYPE_HSPAP:  //api<13 : replace by 15
+                    return "3G";
+                case TelephonyManager.NETWORK_TYPE_LTE:    //api<11 : replace by 13
+                    return "4G";
+                default:
+                    return "?";
+            }
+        }
+        return "?";
     }
 }
