291dfc33c7d4054780991fcead29ba3976b82971
Anartz Mugika <mugan86@gmail.com>
2017-09-25 21:20:06 +0200

    Add connectivity action constant

--- app/src/main/java/anartzmuxika/connectionchangereceiver/Constants.java
+++ app/src/main/java/anartzmuxika/connectionchangereceiver/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public static final String CONNECT_TO_WIFI = "WIFI";
     public static final String CONNECT_TO_MOBILE = "MOBILE";
     public static final String NOT_CONNECT = "NOT_CONNECT";
+    final static String CONNECTIVITY_ACTION = "android.net.conn.CONNECTIVITY_CHANGE";
 }
